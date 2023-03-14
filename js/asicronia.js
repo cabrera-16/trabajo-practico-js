24affd0fe4cd6cd79e6ddc91cf69dafef2e4621a
mi primer git <josemiguelcabrera4@gmail.com>
2023-03-14 15:09:43 -0400

    asyn - await

--- js/asicronia.js
+++ js/asicronia.js
@@ -18,13 +18,13 @@ setInterval(()=>{
 
 clearTimeout(temporizador);
 console.log("depues de cleartimeout");*/
-
+/*
 let temporizador = setInterval(()=>{
     console.log(new Date().toLocaleTimeString())
 },1000);
 
 clearTimeout(temporizador);
-console.log("depues de clearinterval");
+console.log("depues de clear interval");*/
 
 
 /* Ante de explicar como funciona el modelo de javascrits es importante enteder algunos conceptos:
@@ -77,7 +77,7 @@ console.log("FIN")
     console.log("fin")
 }); */
 //callbacks
-function cuadradoCallback(value,callback){
+/*function cuadradoCallback(value,callback){
     setTimeout(()=>{
     callback(value,value*1000);
     },0| Math.random()*100);
@@ -107,4 +107,149 @@ function cuadradoCallback(value,callback){
 
 })
 
-});
+});*/
+
+/*
+Función Callback
+Una función de callback es una función que se pasa a otra función como un argumento, que luego se invoca dentro de la función externa 
+para completar algún tipo de rutina o acción.*/
+// sincrona se ejecuta de inmediatamente 
+/*function saludar(nombre ){
+    alert(` hola mi primer nombre callback ${nombre}`)
+}
+function procesarEntredaUsurio(callback){
+    let nombre = prompt(`por favor ingrese el nombre `);
+    callback(nombre);
+}
+procesarEntredaUsurio(saludar);**/
+
+
+//Promise
+
+/*function cuadradoPromise(value){
+    //intancias de objecto promise resive un funcion resolve y reject. resolve + y reject - ojo  metodo resolver y reject son metodo estatico
+    // costrutor reject catch
+    if(typeof value!="number") return Promise.reject(`error , el valor "${value}" ingresado no es un numero `);
+    return new Promise((resolve,reject)=>{
+        
+    setTimeout(()=>{
+        resolve({
+            value,
+            result:value*value
+        })
+    },0| Math.random()*100);
+
+    });
+}
+cuadradoPromise (0)
+    .then((obj)=>{
+       // console.log(obj);
+       console.log("inicio de promise");
+       console.log(`promise:${obj.value}, ${obj.result}`);
+       return cuadradoPromise(1);
+    })
+    .then((obj)=>{
+       
+        console.log(`promise:${obj.value}, ${obj.result}`);
+        return cuadradoPromise(2);
+     })
+     .then((obj)=>{
+        
+        console.log(`promise:${obj.value}, ${obj.result}`);
+        return cuadradoPromise(3);
+     })
+     .then((obj)=>{
+       
+       console.log(`promise:${obj.value}, ${obj.result}`);
+       return cuadradoPromise(4);
+    })
+    .then((obj)=>{
+       
+       console.log(`promise:${obj.value}, ${obj.result}`);
+       return cuadradoPromise("0");
+    })
+    .then((obj)=>{
+      
+       console.log(`promise:${obj.value}, ${obj.result}`);
+      console.log(`fin de la promise`)
+    })
+    
+    .catch(err=>console.error(err))*/
+
+    //Async - Await 
+
+    function cuadradoPromise(value){
+    //intancias de objecto promise resive un funcion resolve y reject. resolve + y reject - ojo  metodo resolver y reject son metodo estatico
+    // costrutor reject catch
+    if(typeof value!="number") return Promise.reject(`error , el valor "${value}" ingresado no es un numero `);
+    return new Promise((resolve,reject)=>{
+        
+    setTimeout(()=>{
+        resolve({
+            value,
+            result:value*value
+        })
+    },0| Math.random()*100);
+
+    });
+}// esto es una funcion asicrona try  es la parte + y catch - Await es la espera mediante proceso
+async function funcionAsicronaDeclarada(){
+    try {
+        console.log("inicico Async Funcion");
+        let obj = await cuadradoPromise(1);
+        console.log(`Async functions:${obj.value},${obj.result}`);
+        
+         obj = await cuadradoPromise('1');
+        console.log(`Async functions:${obj.value},${obj.result}`);
+
+         obj = await cuadradoPromise(2);
+        console.log(`Async functions:${obj.value},${obj.result}`);
+
+         obj = await cuadradoPromise(3);
+        console.log(`Async functions:${obj.value},${obj.result}`);
+
+         obj = await cuadradoPromise(4);
+        console.log(`Async functions:${obj.value},${obj.result}`);
+
+         obj = await cuadradoPromise(5);
+        console.log(`Async functions:${obj.value},${obj.result}`);
+
+       
+        console.log("fin de Asyn Await");
+        
+        
+    } catch ( error) {
+        console.error(error)
+        
+    }
+}
+  const funcioneAsicronaExpresada =  async ()=>{
+     try {
+        console.log("inicico Async Funcion");
+        let obj = await cuadradoPromise(6);
+        console.log(`Async functions:${obj.value},${obj.result}`);
+        
+         obj = await cuadradoPromise(7);
+        console.log(`Async functions:${obj.value},${obj.result}`);
+
+         obj = await cuadradoPromise("8");
+        console.log(`Async functions:${obj.value},${obj.result}`);
+
+         obj = await cuadradoPromise(9);
+        console.log(`Async functions:${obj.value},${obj.result}`);
+
+         obj = await cuadradoPromise(10);
+        console.log(`Async functions:${obj.value},${obj.result}`);
+
+        console.log("fin de Asyn Await");
+        
+        
+    } catch ( error) {
+        console.error(error)
+        
+    }
+
+
+ }
+funcionAsicronaDeclarada();
+funcioneAsicronaExpresada();
